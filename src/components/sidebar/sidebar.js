c16a4b1e7141b6dc7bf8dda0798e84745b4839da
Chris Dunne <chrisdunne66@gmail.com>
2022-04-11 17:53:58 +0100

    0.7.1 - Cleaning Up Code

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -15,10 +15,13 @@ export const Sidebar = (props) =>{
             setSidebarStatus(false)            
             document.getElementById("sidebar__container").classList.add("hideSidebar")
             document.getElementById("sidebar__container").classList.remove("showSidebar")
+            // document.getElementById("sidebar__container").classList.toggle("showSidebar")
         }else{
             setSidebarStatus(true)
             document.getElementById("sidebar__container").classList.remove("hideSidebar")
             document.getElementById("sidebar__container").classList.add("showSidebar")
+            
+            // document.getElementById("sidebar__container").classList.toggle("hideSidebar")
         }
     }
 
@@ -28,30 +31,30 @@ return(
         <h1>Sidebar</h1>
         <Button className="sidebar__toggleButton" onClick={()=>toggleSidebar()}>
             {displaySidebar === false ? 
-                 <div><span class="material-icons">view_sidebar</span>Show Sidebar</div>
+                 <div className="d-flex"><span class="material-icons">view_sidebar</span>Show Sidebar</div>
                 : 
-                <div><span class="material-icons">close</span>Hide Sidebar</div>
+                <div className="d-flex"><span class="material-icons">close</span>Hide Sidebar</div>
             }
         </Button>
 
-        <div id="sidebar__container" className="sidebar__container">
+        <div id="sidebar__container" className="sidebar__container position-fixed">
 
             <div  className="sidebar__innerContainer">
 
 {/* Menu Header and Toggle Sidebar Button */}
-                <div onClick={()=>toggleSidebar()} id="sidebar__header" className="sidebar__header" >
-                    <div class="sidebar__toggleButton"><span class="material-icons">close</span></div>
+                <div onClick={()=>toggleSidebar()} id="sidebar__header" className="sidebar__header d-flex align-items-center mt-2 mb-3" role="button">
+                    <div class="sidebar__toggleButton m-3"><span class="material-icons">close</span></div>
                     <h2>Sidebar</h2>
                 </div>
 
 {/* The Menu Contents */}
-                <ul className="sidebar__menuContainer">
-                    <li>Colout Flipper</li>
-                    <li>Counter</li>
-                    <li>Carousel</li>
-                    <li>Sidebar</li>
+                <ul className="sidebar__menuContainer ps-2 list-unstyled fs-4">
+                    <li className="p-2" role="button">Colour Flipper</li>
+                    <li className="p-2" role="button">Counter</li>
+                    <li className="p-2" role="button">Carousel</li>
+                    <li className="p-2" role="button">Sidebar</li>
                 </ul>
-                
+
             </div>
 
         </div>
