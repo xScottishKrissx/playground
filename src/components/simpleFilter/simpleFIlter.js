305c9c7f7d7bd899b14dccbe227910d454068775
Chris Dunne <chrisdunne66@gmail.com>
2022-04-13 16:53:29 +0100

    0.10.1 - Filter with mapped buttons and better styling

--- src/components/simpleFilter/simpleFIlter.js
+++ src/components/simpleFilter/simpleFIlter.js
@@ -2,6 +2,8 @@ import * as React from 'react'
 
 import './simpleFilter.css'
 
+
+import { Row,Col } from 'react-bootstrap';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
@@ -10,51 +12,64 @@ export const SimpleFilter = (props) =>{
     const data = [
         {
             name:"Alan",
-            department:"IT"
+            department:"IT",
+            img:"https://randomuser.me/api/portraits/men/1.jpg"
         },
         {
             name:"Bob",
-            department:"IT"
+            department:"HR",
+            img:"https://randomuser.me/api/portraits/men/2.jpg"
+            
         },
         {
             name:"Cara",
-            department:"Marketing"
+            department:"Marketing",
+            img:"https://randomuser.me/api/portraits/women/3.jpg"
         },
         {
             name:"Donna",
-            department:"Legal"
+            department:"Legal",
+            img:"https://randomuser.me/api/portraits/women/4.jpg"
         },
         {
             name:"Edward",
-            department:"IT"
+            department:"IT",
+            img:"https://randomuser.me/api/portraits/men/5.jpg"
         },
         {
             name:"George",
-            department:"Sales"
+            department:"Sales",
+            img:"https://randomuser.me/api/portraits/men/6.jpg"
         },
         {
             name:"Harry",
-            department:"Sales"
+            department:"Sales",
+            img:"https://randomuser.me/api/portraits/men/7.jpg"
         },
         {
             name:"India",
-            department:"Marketing"
+            department:"Marketing",
+            img:"https://randomuser.me/api/portraits/women/8.jpg"
         },
         {
             name:"Jenny",
-            department:"Sales"
+            department:"Sales",
+            img:"https://randomuser.me/api/portraits/women/9.jpg"
         },
         {
             name:"Konnor",
-            department:"Legal"
+            department:"Legal",
+            img:"https://randomuser.me/api/portraits/men/10.jpg"
         },
         {
             name:"Luis",
-            department:"Legal"
+            department:"Legal",
+            img:"https://randomuser.me/api/portraits/men/11.jpg"
         },
         {
             name:"Myrtle",
-            department:"Marketing"
+            department:"Marketing",
+            img:"https://randomuser.me/api/portraits/women/12.jpg"
         }
 
     ]
@@ -66,8 +81,11 @@ export const SimpleFilter = (props) =>{
         // Toggle Modal
         setModalStatus(setModal)
         setOption("All")
+        document.querySelector('body').classList.toggle("lock-scroll")
     }
 
+
+    // Filtering 
     function toggleFilter(option){ setOption(option) }
     
     const filterData = data.filter(x => x.department === option)
@@ -77,18 +95,33 @@ export const SimpleFilter = (props) =>{
 
     const displayInfo = useData.map((x,index) => {
         return(
-            <div key={index}>
-                <p>{x.name} - {x.department}</p>
-            </div>
+            
+                <Col key={index} sm={3} className="d-flex align-items-center justify-content-flex-start p-0 m-2 w-auto"> 
+                    <img src={x.img} alt={x.name} />
+                    <p className="text-center">{x.name} - <strong>{x.department}</strong></p>
+                </Col>
         )
     })
 
+    // Mapping Buttons from data
+    const getDepartmentFromData = data.map(x => x.department)
+    const departmentArray = Array.from(new Set(getDepartmentFromData))
+    departmentArray.unshift("All")
+    departmentArray.sort()
+
+    const mapButtons = departmentArray.map((x,index) => {
+        return(
+            <Button className="ms-2 me-2 ps-4 pe-4" key={index} onClick={()=>toggleFilter(x)}>{x}</Button>
+        )
+    })
+    
+
 
 return(
-    <div className="modal__container">
+    <div className="simpleFilter__container">
 
 {/* Activate Modal */}
-        <div className="modal__activateModal">        
+        <div className="simpleFilter__activateModal">        
             <h1>Simple Filter</h1>
             <Button onClick={()=>toggleModal(true)}>Open Window</Button>
         </div>
@@ -103,19 +136,22 @@ return(
 
             :
             
-            <div className="modal__modalContainer d-flex justify-content-center align-items-center top-0 start-0 position-fixed w-100 h-100"  >
+            <div className="simpleFilter__modalContainer d-flex justify-content-center top-0 start-0 position-fixed w-100 h-100 overflow-auto"  >
                 
-                <div className="modal__innerModalBackground h-100 position-absolute w-100" onClick={()=>toggleModal(false)}></div>
+                <div className="simpleFilter__innerModalBackground h-100 position-absolute w-100" onClick={()=>toggleModal(false)}></div>
 
-                <div className="modal_innerModalContainer p-5 rounded" >
+                <div className="simpleFilter__innerModalContainer p-5 rounded w-100" >
                     <h1>Simple Filter</h1>
-                    <Button onClick={()=>toggleFilter("All")}>All</Button>
-                    <Button onClick={()=>toggleFilter("IT")}>IT</Button>
-                    <Button onClick={()=>toggleFilter("Sales")}>Sales</Button>
-                    <Button onClick={()=>toggleFilter("Legal")}>Legal</Button>
-                    <Button onClick={()=>toggleFilter("Marketing")}>Marketing</Button>
-                    {displayInfo}
-                    <Button onClick={()=>toggleModal(false)}>Close Modal</Button>
+
+                    <div className="simpleFilter__filterButtons"> {mapButtons} </div>
+                   
+                    
+                    <Row className="simpleFilter__staffOutput justify-content-left">
+                        {displayInfo}
+                    </Row>
+                    
+                    <Button onClick={()=>toggleModal(false)}>Close Window</Button>
+                    
                 </div>
 
             </div>
