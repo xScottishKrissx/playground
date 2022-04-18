cff98af28ef420945b66c3ff3ee29546a365506d
Chris Dunne <chrisdunne66@gmail.com>
2022-04-18 13:16:51 +0100

    0.13 - Simple Tabs

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import Questions from './components/questions/questions';
 import SimpleFilter from './components/simpleFilter/simpleFIlter';
 import SimpleVideo from './components/videoBackground/video';
 import Footer from './components/footer/footer';
-
+import Tabs from './components/tabs/tabs';
 
 
 function App() {
@@ -29,7 +29,7 @@ function App() {
       <Container id="app__container" className="mt-5 mb-5">
         
         <Row fluid="true">
-
+          <Col sm={6} lg={6} className="p-0"> <Tabs /> </Col>
           <Col sm={6} lg={6} className="p-0 overflow-hidden position-relative"> <SimpleVideo /> </Col>
           <Col sm={6} lg={6}> <SimpleFilter /> </Col>
           <Col sm={6} lg={6}> <Questions /> </Col>
--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,15 +1,75 @@
 import * as React from 'react'
 
-// import './your.css'
+import { useState } from 'react'
 
-export const StatelessComponentDefault = (props) =>{
+
+import './tabs.css'
+
+export const Tabs = (props) =>{
+
+    const [isActive, setActive] = useState("tab1")
+
+    const data = [
+        {
+            id:"tab1",
+            title: "Tab 1",
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsam, distinctio perferendis rem voluptas molestias culpa ut quasi eligendi sapiente nemo, iste impedit numquam, pariatur optio libero similique illum molestiae?"
+        },
+        {
+            id:"tab2",
+            title: "Tab 2",
+            content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim reiciendis consequatur dignissimos, necessitatibus similique explicabo alias quia repellat impedit culpa. Non fuga eum necessitatibus, nulla ab vero eos rem illo."
+        },
+        {
+            id:"tab3",
+            title: "Tab 3",
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, repellendus explicabo? Qui recusandae cumque debitis! Enim voluptatibus maxime praesentium. Quisquam placeat molestiae natus aperiam dolor in inventore, ad repellendus nisi!"
+        },
+        {
+            id:"tab4",
+            title: "Tab 4",
+            content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi iste suscipit totam voluptatum voluptate quasi perferendis cupiditate dolorum vel temporibus ipsam, ex tempore hic rerum expedita laudantium tenetur! Dolorum, accusamus."
+        },
+
+    ]
+
+    const getTab = data.filter(x => x.id === isActive)
+    const mapTabs = getTab.map(x => {
+        return(
+            <div className="tabs__content">
+                <h3>{x.title}</h3>
+                <p>{x.content}</p>
+            </div>
+        )
+    })
+
+    const mapButtons = data.map (x => {
+        return (
+            
+                <button className={ `${x.id === isActive ? "tabs__button active" : "tabs__button" }`  }  onClick={()=>showTab(x.id)}>{x.title}</button>
+            
+        )
+    })
+
+    function showTab(x){ setActive(x) }
     
 return(
-    <div>
-        <h1>Stateless Component Start</h1>
+    <div className="tabs__container">
+
+        <div class="tabs__header">
+            <h2>Simple Tabs</h2>
+        </div>
+
+        {mapTabs}  
+        
+        <div class="tabs__buttonsContainer">
+            {mapButtons}  
+        </div>
+                  
+
     </div>
 )
 
 }
 
-export default StatelessComponentDefault;
+export default Tabs;
