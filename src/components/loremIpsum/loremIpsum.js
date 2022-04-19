27b98a4b93637e2cc41a8c7b20b47713df217445
Chris Dunne <chrisdunne66@gmail.com>
2022-04-19 17:33:41 +0100

    0.15.2 - Lorem Ipsum with user input

--- src/components/loremIpsum/loremIpsum.js
+++ src/components/loremIpsum/loremIpsum.js
@@ -1,53 +1,81 @@
-import { text } from 'dom-helpers'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import * as React from 'react'
 
 import './loremIpsum.css'
-
 import * as textFile from './text.js'
 
 export const LoremIpsum = (props) =>{
 
-    const [textArray, setTextArray] = useState(false)
+
+    useEffect(()=>{
+        generateText(userInput)
+    },[])
+    
+    const [storeTextArray, setTextArray] = useState(false)
+    const [userInput, setUserInput] = useState(1)
+    const [showErrorMsg,setErrorMsgStatus] = useState(false)
     
     const text = textFile.text
-
-    
     function generateText(x){
 
         let textArray = []
-
         for (let i = 0; i < x; i++) {
             let r = randomNumber(text.length)
             const getText = text[r]
             textArray.push(getText)
         }
-        
-        setTextArray(textArray)
+
+        const mapText = textArray.map((x,index)=>{
+            return(
+                <div className="loremIpsum__singleParagraph" key={index}>
+                    <p>{x}</p>
+                </div>
+            )
+        })
+
+        setTextArray(mapText)
     }
 
     function randomNumber(arrayLength){
         return Math.floor((Math.random() * arrayLength))
     }
 
-console.log(textArray)
+    function collectUserInput(input){
 
-const mapText = textArray.map(x=>{
-    return(
-        <div>
-            <p>{x}</p>
-        </div>
-    )
-})
-return(
-    <div>
-        <h2>LoremIpsum</h2>
+        const convertValue = parseInt(input.target.value)
 
-        <button onClick={()=>generateText(3)}>submit</button>
+        if(Number.isInteger(convertValue) && convertValue > 0 && convertValue <= 100){
+            setUserInput(convertValue)
+            setErrorMsgStatus(false)
+        }else{
+            setUserInput(0)
+            setErrorMsgStatus(true)
+        }       
+    }
+    
+        return(
+            <div className="loremIpsum__container">
+                <h2 className="text-center">Lorem Ipsum Generator</h2>
 
-        {textArray ? mapText : null}
-    </div>
-)
+                <div className="loremIpsum__formContainer d-flex pt-3">
+                    <form className="loremIpsum__form" onSubmit={e => e.preventDefault()}>
+                        <label  htmlFor="number">Required Number of Paragraphs</label>
+                        <input name="number" type="text" onInput={(e) => collectUserInput(e)} placeholder="1"/>
+                    </form>
+
+                    <button onClick={()=>generateText(userInput)}>Generate</button>
+                </div>
+
+
+                <div className="loremIpsum__paragraphsContainer pt-3">
+                    {storeTextArray ? storeTextArray : null}
+                </div>
+
+                {showErrorMsg ? <div className="loremIpsum__errorMessage">Enter a valid number please(1 to 100)</div> : null }
+                
+                
+            </div>
+        )
 
 }
 
