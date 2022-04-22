eae856e56001920ed8fd68be3110dbde2c64d5c2
Chris Dunne <chrisdunne66@gmail.com>
2022-04-22 14:50:21 +0100

    0.16.8 - Cleaned up css

--- src/components/simpleTodo/simpleTodo.js
+++ src/components/simpleTodo/simpleTodo.js
@@ -17,70 +17,81 @@ export const SimpleTodoFunctional = () =>{
     })
 
 // References
+    const emptyFieldWarning = useRef(false)
     const formInput = useRef(null)
     const confirmDeleteAll = useRef(false)
 
 // Add Item
     const addItem = () => {
-        
-        console.log("Add New Item");
+        // console.log("Add New Item");
+
         toggleEditMode(false)
         const value = formInput.current.value     
         if(value.length > 1){
             let itemsArray = items || [] 
-            itemsArray.push(value)  
-            setItem(items => ([...items]))    
+            itemsArray.push(value)
+            setItem(items => ([...items]))
+            setFormValue(null)      
             setLocalStorage(items)
-            setFormValue(null)
-        }return;
+            toggleEmptyFieldWarning()
+        }else{
+            toggleEmptyFieldWarning(true)
+        }
     }
 
 // Edit
-    const startEdit = (index) => {
-        console.log("Start Edit");
-        const edit = getLocalStorage[index]
-        setFormValue(edit)
+    const startEdit = (index,e) => {
+        // console.log("Start Edit");
+
+        setFormValue(getLocalStorage[index])
         setEditState({editMode:true,editIndex:index})
+        toggleEmptyFieldWarning()
     }
 
     const cancelEdit = () => {
         setFormValue(null)
         toggleEditMode(false)
+        
     }
     const submitEdit =() => {        
 
         const newValue = formInput.current.value
         if(newValue.length < 1) {
             toggleEditMode(false)
-            alert("Error: Field cannot be empty")
+            toggleEmptyFieldWarning(true)
             return
         }
 
-        const valueToChange = edit.editIndex
-        getLocalStorage.splice(valueToChange,1,newValue)
+        getLocalStorage.splice(edit.editIndex,1,newValue)
+        setItem(getLocalStorage) 
 
         setFormValue(null)
-        setItem(getLocalStorage) 
         toggleEditMode(false)
+        toggleEmptyFieldWarning()
         setLocalStorage(getLocalStorage)
-        
-
     }
 
 // Delete
     const handleDelete = (itemIndex, numToDelete) => {
-
-        getLocalStorage.splice(itemIndex,numToDelete)
-
         if(numToDelete > 1) toggleConfirmDeleteAllBox()
 
+        getLocalStorage.splice(itemIndex,numToDelete)      
+
         setFormValue(null)
-        setItem(getLocalStorage) 
         toggleEditMode(false)
+        toggleEmptyFieldWarning()
+        setItem(getLocalStorage) 
         setLocalStorage(getLocalStorage)
     }
 
 // Utility Functions
+    const toggleEmptyFieldWarning = (setVisible) =>{
+        if(setVisible)emptyFieldWarning.current.classList.add("show")
+        if(!setVisible)emptyFieldWarning.current.classList.remove("show")       
+    }
+
+
+
     const setLocalStorage = (toBeStored) =>  localStorage.setItem("items",JSON.stringify(toBeStored))
     const toggleEditMode = (setEditModeTo) => setEditState({editMode:setEditModeTo}) 
     const setFormValue = (setFormValueTo) => formInput.current.value = setFormValueTo
@@ -89,13 +100,13 @@ export const SimpleTodoFunctional = () =>{
 // Display the items
     const mapItems = items.map((x,index) => {
         return(
-            <div key={index} className="simpleTodo__singleItem d-flex">
+            <div key={index} className="simpleTodo__singleItem d-flex mt-3">
 
-                <p className="w-75">{x}</p>
+                <p className="w-75 m-0 p-0">{x}</p>
 
-                <div  className="simpleTodo__singleItemButtons w-25">
-                    <span role="button" onClick={()=>startEdit(index)} className="material-icons simpleTodo__editButton">edit</span>
-                    <span role="button" onClick={()=>handleDelete(index, 1)} className="material-icons simpleTodo__deleteButton">delete</span>
+                <div  className="simpleTodo__singleItemButtons d-flex justify-content-evenly align-items-center w-25">
+                    <span role="button" onClick={(e)=>startEdit(index,e)} className="material-icons simpleTodo__editButton text-info">edit</span>
+                    <span role="button" onClick={()=>handleDelete(index, 1)} className="material-icons simpleTodo__deleteButton text-danger">delete</span>
                 </div>
 
                 
@@ -105,28 +116,42 @@ export const SimpleTodoFunctional = () =>{
 
     return(
 
-        <div className="simpleTodo__container"> 
+        <div className="simpleTodo__container d-flex flex-column justify-content-center align-items center h-100 w-100 position-relative"> 
+
+
 
     {/* Delete All Button */}
         {items.length > 1 ?
 
-            <div className="simpleTodo__deleteAllContainer">
-                <button className="simpleTodo__deleteAllButton"  onClick={()=>toggleConfirmDeleteAllBox()}>
+            <div className="simpleTodo__deleteAllContainer mb-4 top-0 w-100 ">
+                
+                <button className="simpleTodo__deleteAllButton border-0 d-flex align-items-center justify-content-center p-2 w-100"  onClick={()=>toggleConfirmDeleteAllBox()}>
                     <span className="material-icons">delete_sweep</span>Delete All
                 </button>
 
-                <div className="simpleTodo__confirmDeleteAllContainer " ref={confirmDeleteAll} >
-                    <span id="simpleTodo__confirmDeleteAllButton" role="button" onClick={()=>handleDelete('',items.length)}>Confirm</span>
-                    <span id="simpleTodo__confirmCancelButton" role="button" onClick={toggleConfirmDeleteAllBox}>Cancel</span>
+                <div className="simpleTodo__confirmDeleteAllContainer border-0 align-items-center justify-content-evenly w-100" ref={confirmDeleteAll} >
+                    <span className="bg-danger text-white" id="simpleTodo__confirmDeleteAllButton" role="button" onClick={()=>handleDelete('',items.length)}>Confirm</span>
+                    <span className="bg-secondary text-white" id="simpleTodo__confirmCancelButton" role="button" onClick={toggleConfirmDeleteAllBox}>Cancel</span>
                 </div>
             </div>
 
         :null}  
             
             <h2>Simple To-Do</h2>
+
+    {/* Empty Field Warning Message */}
+                <div ref={emptyFieldWarning} className="simpleTodo__emptyFieldWarning">
+                    <p className="d-flex">
+                        <span className="material-icons">priority_high</span>
+                        <strong>oops, this field <span className="text-decoration-underline">cannot</span> be empty</strong>
+                    </p>
+                </div>
+
     {/* List Item Entry */}
-            <div className="simpleTodo__formContainer">
-                <form className="simpleTodo__form">
+            <div className="simpleTodo__formContainer d-flex w-100">
+
+
+                <form className="simpleTodo__form border-0 lh-lg w-100">
                     <label htmlFor="itemEntry"></label>
                     <input ref={formInput} type="text" name="itemEntry" />
                 </form>
@@ -143,7 +168,7 @@ export const SimpleTodoFunctional = () =>{
             </div>
 
     {/* The To Do List */}
-            <div className="simpleTodo__list"> {mapItems} </div>
+            <div className="simpleTodo__list w-100"> {mapItems} </div>
             
         </div>
     )
