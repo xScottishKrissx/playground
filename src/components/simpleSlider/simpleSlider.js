9a78df1a4d3b451850b47861dc8b0486b7483782
Chris Dunne <chrisdunne66@gmail.com>
2022-04-25 19:00:04 +0100

    0.17.1 - Slider with transition and controls
    Mostly happy with this but I think i can improve on this

--- src/components/simpleSlider/simpleSlider.js
+++ src/components/simpleSlider/simpleSlider.js
@@ -9,33 +9,66 @@ import mountain from './mountain.jpg'
 
 export const SimpleSlider = (props) =>{
 
-    const [test, setTest] = useState(0)
-    const slider = useRef()
+    const [slidePosition, setSlidePosition] = useState(0)
+    const [count, setCount] = useState(0)
 
-    const next = () =>{ setTest(test - 100 ) }
-    const prev = () =>{ setTest(test + 100 ) }
-    
+    let currentCount = count
+    const prev = (length) =>{
+
+        if(count === 0){
+            setSlidePosition(slidePosition - 100 * (length - 1))
+            setCount(length - 1)
+            return 
+        }
+
+        currentCount--;
+        setSlidePosition(slidePosition + 100 ) 
+        setCount(currentCount)
+    }
+
+    const next = (length) =>{
+        if(count > length - 2) {
+            setSlidePosition(0)
+            setCount(0)
+            return
+        }
+
+        currentCount++;
+        setCount(currentCount)
+        setSlidePosition(slidePosition - 100 ) 
+
+    }
+
+    const style = { transform:'translateX(' + slidePosition + '%)' }
+    // THis is how i mapped images. Might be a better way but this is what i used
     const images = ["woods","cliffs", "mountain"]
     const mapImages = images.map ((x,index) => {
         return(
-                <img key={index} alt={x} src={require('./' + x + '.jpg')} />            
+                <img className="w-100" key={index} alt={x} src={require('./' + x + '.jpg')} />                
         )
     })
 
+    // const mapImages = images.map ((x,index) => {
+    //     return(
+    //         <div>
+    //             <img className="w-100" key={index} alt={x} src={require('./' + x + '.jpg')} />   
+    //         </div>             
+    //     )
+    // })
     
-    const style = {
-        transform:'translateX(' + test + '%)',
-    }
 
-return(
-    <div className='simpleSlider__container'>  
-        <h2>Slider</h2>
-        <div ref={slider} style={style} className={`simpleSlider__sliderWrapper ${test}`}>{mapImages}</div>
+    return(
+        <div className='simpleSlider__container'>  
+            <h2>Slider</h2>
 
-        <button onClick={()=>next()}>Next</button>
-        <button onClick={()=>prev()}>Prev</button>
-    </div>
-)
+            <div style={style} className="simpleSlider__sliderWrapper" >{mapImages}</div>
+
+            <div className="simpleSlider__buttonContainer d-flex justify-content-center align-items-center p-3">
+                <button className='m-1 w-50' onClick={()=>prev(images.length)}><span class="material-icons">arrow_back</span></button>
+                <button className="m-1 w-50" onClick={()=>next(images.length)}><span class="material-icons">arrow_forward</span></button>
+            </div>
+        </div>
+    )
 
 }
 
