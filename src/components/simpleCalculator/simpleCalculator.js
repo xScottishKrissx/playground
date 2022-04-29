9efb2b3a81f21ddd6a73035c3c1796c86f27cacc
Chris Dunne <chrisdunne66@gmail.com>
2022-04-29 15:58:47 +0100

    1.1.5 - Simple Calculator
    I have now styled the calculator and it's looking quite good.
    I have also changed the display to show the working of the answer.
    --Removed the clear entry button as i was getting a bit confused with certain aspects of the issue. I might add this back in later.

--- src/components/simpleCalculator/simpleCalculator.js
+++ src/components/simpleCalculator/simpleCalculator.js
@@ -18,12 +18,15 @@ export const SimpleCalculator = (props) =>{
     const [previousAnswer, setPrevAnswer] = useState(0)
 
     // Display
-    const [displayAnswer, setDisplay] = useState(0)
+    const [currentAnswer, setAnswer] = useState(null)
     
     const action = (x) => {
-        console.log(previousAnswer)
-        console.log(currentNumber)
-        console.log(previousNumber)
+        
+        setAnswer(null)
+        showAnswer()
+        // console.log(previousAnswer)
+        // console.log(currentNumber)
+        // console.log(previousNumber)
         
         // Don't do anydisplayOperator if first input is 0
         //// This should be changed to add a decimal after pressing 0, so 0 => .0
@@ -51,6 +54,7 @@ export const SimpleCalculator = (props) =>{
         //Set the operator into state, to be used in the getAnswer function 
         setOperator(operator)
 
+        setAnswer(null)
         // Set the previous number value as the last answer if it exists.
         //// This is important for allowing consecutive answers
         if(previousAnswer !== 0){
@@ -72,7 +76,7 @@ export const SimpleCalculator = (props) =>{
         const answer = Calculate(previousNumber,operator,parseFloat(currentNumber))
         
         // Display the answer on page
-        setDisplay(answer)
+        setAnswer(answer)
         // Set the answer to the current equation as the previous answer, to be used in consecutive equations
         setPrevAnswer(answer)
     }
@@ -87,81 +91,118 @@ export const SimpleCalculator = (props) =>{
     
 
     const clearAll = () => {
-        console.log("Clear All")
+        // console.log("Clear All")
         setCurrentNumber(0)
         setAsPreviousNumber(0)
         setPrevAnswer(0)
-        setDisplay(0)
+        setAnswer(null)
         setOperator(0)
     }
 
     const clearEntry = () => {
-        console.log("Clear Entry")
+        // console.log("Clear Entry")
         setCurrentNumber(0)
     }
 
-    const showWorking = () =>{
+    const showAnswer = () =>{
         let displayOperator;
         if(operator === "add") displayOperator = "+"
         if(operator === "subtract") displayOperator = "-"
         if(operator === "divide") displayOperator = "/"
         if(operator === "multiply") displayOperator = "*"
+        
+        let formatCurrentNumber = currentNumber === 0 ? "" : currentNumber
+        // console.log(formatCurrentNumber)
 
-        let showWork = "" + previousNumber + " " + displayOperator + " " + currentNumber;
+        let formatAnswer = currentAnswer === null ? "" : " = " + currentAnswer
+        let showWork = "" + previousNumber + " " + displayOperator + " " + formatCurrentNumber + formatAnswer;
+
+        // console.log(currentAnswer)
         return showWork
     }
 
 
 return(
     <div className='simpleCalculator__container'>
-        <h1>SimpleCalculator</h1>
+        {/* <h1>SimpleCalculator</h1> */}
 
         <div className='simpleCalculator_calculator'>
-            {operator ? showWorking() :null}
-            <div className="simpleCalculator__display">{displayAnswer || currentNumber || 0}</div>
-            
-            
-            <p>Previous Number: {previousNumber}</p>
-            <p>Current Number: {currentNumber}</p>
-            <p>Previous Answer: {previousAnswer}</p>
 
-            <div className="simpleCalculator__options">
-                <button onClick={()=>clearAll()}>All Clear</button>
-                <button onClick={()=>clearEntry()}>Clear Entry</button>
+            
+            
+            <div className="simpleCalculator__row1">
+                {/* <div className="showAnswer">{operator ? showAnswer() :<p>Working</p>}</div> */}
+                <div className="simpleCalculator__display">
+
+                {operator ? showAnswer() : currentNumber || 0}
+                    {/* {currentAnswer || currentNumber || 0} */}
+                </div>
+            </div>
+
+            <div className="simpleCalculator__row2">
+                <div className="simpleCalculator__operators">
+                    <button onClick={()=>getOperator("add")}>+</button>
+                    <button onClick={()=>getOperator("subtract")}>-</button>
+                    <button onClick={()=>getOperator("divide")}>/</button>
+                    <button onClick={()=>getOperator("multiply")}>*</button>
+                </div>
+            </div>
+
+            <div className="simpleCalculator__row3">
+
+                <div className='simpleCalculator__numbers'>
+                    <button id="solutionButton" onClick={()=>getAnswer()}>=</button>
+                    <button onClick={()=>action("1")}>1</button>
+                    <button onClick={()=>action("2")}>2</button>
+                    <button onClick={()=>action("3")}>3</button>
+                    <button onClick={()=>action("4")}>4</button>
+                    <button onClick={()=>action("5")}>5</button>
+                    <button onClick={()=>action("6")}>6</button>
+                    <button onClick={()=>action("7")}>7</button>
+                    <button onClick={()=>action("8")}>8</button>
+                    <button onClick={()=>action("9")}>9</button>
+                    <button onClick={()=>action("0")}>0</button>
+                    {/* There be monsters */}
+                    <button onClick={()=>action("decimal")}>.</button>
+                    {/* <button onClick={()=>clearEntry()}>CE</button> */}
+                    <button id="clearAllButton" onClick={()=>clearAll()}>AC</button>
+                    
+                </div>
+
+                {/* <div className="simpleCalculator__solution">
+                    <button onClick={()=>getAnswer()}>=</button>
+                </div> */}
+            </div>
+           
+
+                
+
+   
+
+
+
+                
+
+
+           
+
+
     
 
 
-            </div>
 
-            <div className='simpleCalculator__numbers'>
-                <button onClick={()=>action("1")}>1</button>
-                <button onClick={()=>action("2")}>2</button>
-                <button onClick={()=>action("3")}>3</button>
-                <button onClick={()=>action("4")}>4</button>
-                <button onClick={()=>action("5")}>5</button>
-                <button onClick={()=>action("6")}>6</button>
-                <button onClick={()=>action("7")}>7</button>
-                <button onClick={()=>action("8")}>8</button>
-                <button onClick={()=>action("9")}>9</button>
-                <button onClick={()=>action("0")}>0</button>
-            </div>
 
-            <div className="simpleCalculator__operators">
-                <button onClick={()=>getOperator("add")}>+</button>
-                <button onClick={()=>getOperator("subtract")}>-</button>
-                <button onClick={()=>getOperator("divide")}>/</button>
-                <button onClick={()=>getOperator("multiply")}>*</button>
-                {/* There be monsters */}-
-                <button onClick={()=>action("decimal")}>.</button>
-            </div>
+
+
             
         
-            <div className="simpleCalculator__solution">
-                <button onClick={()=>getAnswer()}>=</button>
-            </div>
+
 
         </div>
 
+            {/* <p>Previous Number: {previousNumber}</p>
+            <p>Current Number: {currentNumber}</p>
+            <p>Previous Answer: {previousAnswer}</p> */}
     </div>
 )
 
