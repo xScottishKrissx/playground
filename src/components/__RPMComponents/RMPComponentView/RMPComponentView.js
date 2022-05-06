1b346a2580a004ac9ea03d5a8f6454d97bfd16f1
Chris Dunne <chrisdunne66@gmail.com>
2022-05-06 12:52:51 +0100

    1.3.4 - Added github link to component view

--- src/components/__RPMComponents/RMPComponentView/RMPComponentView.js
+++ src/components/__RPMComponents/RMPComponentView/RMPComponentView.js
@@ -5,6 +5,8 @@ import './RMPComponentView.css'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+import githubLogo  from './githubLogo.png'
+
 import { components } from '../utilities/componentList'
 import RMPSearchComponents from '../RMPSearchComponents/RMPSearchComponents'
 
@@ -16,7 +18,17 @@ export default function RMPComponentView(props) {
     )
     const mapComponents = filterComps.map((x,index) => {
       return(
-          <Col key={index} sm={12} lg={6} className={x.name + "__componentContainer"}>  <x.componentName /> </Col>
+          <Col key={index} sm={12} lg={6} className={x.name + "__componentContainer position-relative"}>  
+            <x.componentName /> 
+            <div className='component_githubLinkContainer'>
+              <div className='componentGithubLink'>
+                <a href={x.github} target="__top">
+                  <img alt="View code on github.com" src={githubLogo} />
+                </a>
+
+              </div>
+            </div>
+          </Col>
       )
     })
     
--- src/components/__RPMComponents/utilities/componentList.js
+++ src/components/__RPMComponents/utilities/componentList.js
@@ -20,82 +20,97 @@ export const components = [
     {
       name:"SimpleCalculator",
       componentName:SimpleCalculator,
-      tags:"useState hook, bootstrap, props, lifting state, toString, includes, if, parseFloat, conditional rendering,  " 
+      tags:"useState hook, bootstrap, props, lifting state, toString, includes, if, parseFloat, conditional rendering,  " ,
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/simpleCalculator"
     },
 
     {
       name:"Slider",
       componentName:Slider,
-      tags:"useEffect hook, useState hook, useRef hook, setInterval, addEventListener, resize, client height, clearInterval, if, map, image, bootstrap, " 
+      tags:"useEffect hook, useState hook, useRef hook, setInterval, addEventListener, resize, client height, clearInterval, if, map, image, bootstrap, " ,
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/simpleSlider"
     },
 
     {
       name:"SimpleTodo",
       componentName:SimpleTodo,
-      tags:"useState hook, useEffect hook, functional, local storage, json, parse, getItem,toggle, push, splice, setItem, conditional rendering, classList, bootstrap, lifting state, forwardRef,props, lifting props," 
+      tags:"useState hook, useEffect hook, functional, local storage, json, parse, getItem,toggle, push, splice, setItem, conditional rendering, classList, bootstrap, lifting state, forwardRef,props, lifting props," ,
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/simpleTodo"
     },
 
     {
       name:"LoremIpsum",
       componentName:LoremIpsum,
-      tags:"useState hook, useEffect hook, json, for, randomNumber, push, map, math, floor,random, parseInt, Number, isInteger, bootstrap, form, input, conditional rendering"
+      tags:"useState hook, useEffect hook, json, for, randomNumber, push, map, math, floor,random, parseInt, Number, isInteger, bootstrap, form, input, conditional rendering",
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/loremIpsum"
     },
     {
       name:"Countdown",
       componentName:Countdown,
-      tags:"useState hook, useEffect hook, date, getFullYear, getMonth, getDate, getHours,toLocaleDateString, getTime,useEffect hook, setInterval, math, floor, clearInterval,bootstrap, conditional rendering" 
+      tags:"useState hook, useEffect hook, date, getFullYear, getMonth, getDate, getHours,toLocaleDateString, getTime,useEffect hook, setInterval, math, floor, clearInterval,bootstrap, conditional rendering",
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/countdown" 
     },
     {
       name:"Tabs",
       componentName:Tabs,
-      tags:"useState hook, json, filter, map, conditional rendering, functional, bootstrap"
+      tags:"useState hook, json, filter, map, conditional rendering, functional, bootstrap",
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/tabs"
     },
     {
       name:"SimpleVideo",
       componentName:SimpleVideo,
-      tags:"useRef hook, useState hook, video, pause, play, bootstrap, conditional rendering" 
+      tags:"useRef hook, useState hook, video, pause, play, bootstrap, conditional rendering" ,
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/videoBackground"
     },
 
     {
       name:"SimpleFilter",
       componentName:SimpleFilter,
-      tags:"bootstrap, useState hook, toggle, querySelector, filter, if, conditional rendering, map, from, unshift, sort,json" 
+      tags:"bootstrap, useState hook, toggle, querySelector, filter, if, conditional rendering, map, from, unshift, sort,json" ,
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/simpleFilter"
     },
     {
       name:"Questions",
       componentName:Questions,
-      tags:"functional, useState hook, conditional rendering, map, bootstrap, toggle  " 
+      tags:"functional, useState hook, conditional rendering, map, bootstrap, toggle  ",
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/questions" 
     },
     {
       name:"SimpleSearch Bar",
       componentName:SimpleSearchBar,
-      tags:"json, props, functional, map, toggle, class list, useRef hook, bootstrap, useState hook, e target value, toLowerCase, includes, conditional rendering" 
+      tags:"json, props, functional, map, toggle, class list, useRef hook, bootstrap, useState hook, e target value, toLowerCase, includes, conditional rendering",
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/simpleSearchBar" 
     },
 
     {
       name:"Modal",
       componentName:Modal,
-      tags:"useState hook, toggle, bootstrap, toggle, conditional rendering" 
+      tags:"useState hook, toggle, bootstrap, toggle, conditional rendering" ,
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/modal"
     },
     {
       name:"Sidebar",
       componentName:Sidebar,
-      tags:"functional, state, useState hook, bootstrap, toggle, ternary, conditional rendering" 
+      tags:"functional, state, useState hook, bootstrap, toggle, ternary, conditional rendering" ,
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/sidebar"
     },
     {
       name:"ReviewCarousel",
       componentName:ReviewCarousel,
-      tags:"stateful, state, randomNumber, math, floor, bootstrap, random user api, image "
+      tags:"stateful, state, randomNumber, math, floor, bootstrap, random user api, image ",
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/reviewCarousel"
     },
     {
       name:"Counter",
       componentName:Counter,
-      tags:"stateful, state, bootstrap"
+      tags:"stateful, state, bootstrap",
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/counter"
     },
     {
       name:"ColourFlipper",
       componentName:ColourFlipper,
-      tags:"Stateful, for loop, state, random, clipboard, bootstrap, material"
+      tags:"Stateful, for loop, state, random, clipboard, bootstrap, material",
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/colourFlipper"
     },
 
 
