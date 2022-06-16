13a28c6880ee92116187845753e46ec9916bde43
Chris Dunne <chrisdunne66@gmail.com>
2022-06-16 17:00:20 +0100

    1.10.35.3 - Added forgotten github link

--- src/components/__RPMComponents/utilities/componentList.js
+++ src/components/__RPMComponents/utilities/componentList.js
@@ -36,7 +36,7 @@ export const components = [
       name:"Covid19Dashboard",
       componentName:Covid19Dashboard,
       tags:"fetch, api, slider, then, state, destructuring, props, media queries, bootstrap, inline styles, leaflet, maps, search, local storage, useRef, setView, flyTo, animation, require, lifting props, format numbers, toString, replace, setItem, getItem, map, entries, reverse, filter, onClick, toLowerCase, onMouseDown, onMouseUp, react chart js 2, graphs, object to array, line chart, onChange, force update, ternary operator, date, toLocaleDateString, react-leaflet       ",
-      github:"",
+      github:"https://github.com/xScottishKrissx/playground/tree/master/src/components/covid19Dashboard",
       iconColour:"dark",
       information:"The Covid 19 dashboard is an escalation of SimpleChart and builds upon everything it did. There are three sections, each outputting data from an API supplied by disease.sh. You can view each country's covid data by clicking on the map marker, searching the map using the search box or the country lookup section. You can also favourite a country so that it will be the default country you see on load and when you click on the gold marker on the map. You also get graphs created with react-chartjs-2 alongside the map, built using react-leaflet, although only for certain countries due to limitations of some data from the API.",
       size:"wide"
