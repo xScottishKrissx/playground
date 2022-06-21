8a81818869868c489d898cba1ef7fa75dbab72ee
Chris Dunne <chrisdunne66@gmail.com>
2022-06-21 15:56:24 +0100

    1.12.1 - Knight on Board

--- src/components/chessTutorial/chessTutorial.js
+++ src/components/chessTutorial/chessTutorial.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './chessTutorial.css'
-import Knight from './knight'
+import Board from './components/board'
+import Knight from './components/knight'
+import Square from './components/square'
 
 export default function ChessTutorial() {
   return (
     <div className='chessTutorial__wrapper'>
-        <Knight />
+        <Board knightPosition={[7,2]}/>
     </div>
   )
 }
