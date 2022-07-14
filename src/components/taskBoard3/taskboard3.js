536f09c973f7115968582c941000c5f9be796cfc
Chris Dunne <chrisdunne66@gmail.com>
2022-07-14 16:08:23 +0100

    1.13.31 - Tutorial Complete...
    ...now to get back to my original idea and to spin this into my own work

--- src/components/taskBoard3/taskboard3.js
+++ src/components/taskBoard3/taskboard3.js
@@ -33,17 +33,58 @@ export default function Taskboard3() {
 
     
 
-    const onDropEnd = (result) =>{
+    const onDropEnd = (result, columns, setColumns) =>{
+        if(!result.destination) return;
+        const {source, destination} = result
+        if(source.droppableId !== destination.droppableId){
+            const sourceColumn = columns[source.droppableId]
+            const destColumn = columns[destination.droppableId]
 
+            const sourceItems = [...sourceColumn.items]
+            const destItems = [...destColumn.items]
+
+            const [removed] = sourceItems.splice(source.index, 1)
+            destItems.splice(destination.index, 0, removed)
+            
+            setColumns({
+                ...columns,
+                [source.droppableId]:{
+                    ...sourceColumn,
+                    items: sourceItems
+                },
+                [destination.droppableId]:{
+                    ...destColumn,
+                    items: destItems
+                }
+            })
+
+        } else{
+            const column = columns[source.droppableId]
+            const copiedItems = [...column.items]
+    
+            const [removed] = copiedItems.splice(source.index, 1)
+            copiedItems.splice(destination.index, 0, removed)
+    
+            setColumns({
+                ...columns,
+                [source.droppableId]:{
+                    ...columns,
+                    items:copiedItems
+                }
+            })
+        }
     }
 
     return (
         <div className='taskboardWrapper'>
-            <DragDropContext onDropEnd={onDropEnd}>
+            <DragDropContext onDragEnd={(result)=>onDropEnd(result, columns, setColumns)}>
             {/* {newThing} */}
             {Object.entries(columns).map(([id, column])=>{
                 return(
-                    <Droppable droppableId={id}>
+                    <div style={{margin:8}}>
+                    <h2>{column.name}</h2>
+                    <Droppable droppableId={id} key={id}>
+                       
                         {(provided, snapshot) =>{
                             return(
                                 <div 
@@ -82,11 +123,12 @@ export default function Taskboard3() {
                                             </Draggable>
                                         )
                                     })}
-
+                                    {provided.placeholder}
                                 </div>
                             )
                         }}
                     </Droppable>
+                    </div>
                 )
             })}
 
