3d2994d144f715dea120274ae0604daeebcdfbda
Chris Dunne <chrisdunne66@gmail.com>
2022-07-21 11:59:01 +0100

    1.13.55 - WOrking on Item WIndow UI
    ...trying to add an indication that the description is being saved as you type, but the onFocus event is behaving strangely, covering the entire window as opposed to just the form element, where it's attached.

--- src/components/taskboard/components/ColumnView/ItemView/ItemWindow/itemWindow.js
+++ src/components/taskboard/components/ColumnView/ItemView/ItemWindow/itemWindow.js
@@ -1,13 +1,22 @@
-import React,{useRef} from 'react'
+import React,{useRef, useState} from 'react'
+
+import loading from '../../../../spinner.gif'
 
 export default function ItemWindow({itemWindowState, item, closeItemWindow, columnId, addDescription}) {
 
     const {id, content, description} = item
     
     const addDescriptionForm = useRef()
+    const[isFormFocused, setIsFormFocused] = useState(false)
     
     const addNewDescription = () =>{
         const formValue = addDescriptionForm.current.value
+
+        // Do something with form when typing
+        console.log(addDescriptionForm)
+        if(document.activeElement === addDescriptionForm.current){
+            console.log("Do Something")
+        }
 
         // if(formValue.length < 1) return
 
@@ -15,33 +24,38 @@ export default function ItemWindow({itemWindowState, item, closeItemWindow, colu
         addDescriptionForm.current.value = ""
     }
 
-    console.log(description.length)
     
     return (
         <>
 
             {itemWindowState.itemId === id ? 
                 
-                <div className='itemWindow'>
+                <div className='itemWindow' >
                     <button onClick={closeItemWindow}>Close</button>
                     <h2>{content}</h2>    
                     {/* <p>{description}</p> */}
 
                     <hr />
                     {description.length > 0 ?  
-                    <>
-                        <form>
-                            <textarea ref={addDescriptionForm} onChange={addNewDescription} value={description} />
-                        </form>
-                        {/* <button onClick={addNewDescription}>Edit Description</button> */}
-                    </>
+                        <>
+                            <form  >
+                                <textarea onClick={()=>setIsFormFocused(true)} ref={addDescriptionForm} onChange={addNewDescription} value={description} />
+                            </form>
+
+                            { isFormFocused ? 
+                                <span onClick={()=>setIsFormFocused(false)}><img src={loading} />Saving</span> 
+                                : 
+                                <span >Description Saved</span> 
+                            }
+
+                        </>
                     : 
-                    <>
-                        <form>
-                            <textarea aria-controls='none' ref={addDescriptionForm} placeholder="Add Description"/>
-                        </form>
-                        <button onClick={addNewDescription}>Add Description</button>
-                    </>
+                        <>
+                            <form>
+                                <textarea aria-controls='none' onChange={addNewDescription} ref={addDescriptionForm} placeholder="Add Description"/>
+                            </form>
+                            <span>Add Description</span>
+                        </>
                     }
                 </div>
 
