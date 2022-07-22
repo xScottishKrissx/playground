4dbff9b31eda08acefe6812e6c3a1be7b9f72ed5
Chris Dunne <chrisdunne66@gmail.com>
2022-07-22 16:53:22 +0100

    1.13.60.1 - More work on Change Title UI

--- src/components/taskboard/components/ColumnView/ItemView/ItemWindow/itemWindow.js
+++ src/components/taskboard/components/ColumnView/ItemView/ItemWindow/itemWindow.js
@@ -1,35 +1,23 @@
 import React,{useRef, useState, useEffect} from 'react'
 
-
-
 export default function ItemWindow({itemWindowState, item, closeItemWindow, columnId, addDescription, updateTitle}) {
 
     const {id, content, description} = item
 
-
-    
     const addDescriptionForm = useRef()
     const changeTitleForm = useRef()
     const itemWindow = useRef()
     
-    const addNewDescription = (fieldToChange) =>{
-        console.log(fieldToChange)
-        // turnOffDrag(true)
+    const addNewDescription = () =>{
         const formValue = addDescriptionForm.current.value
-        addDescription(formValue, columnId, itemWindowState.itemId, fieldToChange)
-        addDescriptionForm.current.value = ""
+        addDescription(formValue, columnId, itemWindowState.itemId)
     }
 
     const changeTitle = () =>{
         const formValue = changeTitleForm.current.value
         updateTitle(formValue, columnId, itemWindowState.itemId)
-        console.log(formValue)
     }
 
-
-
-    
-    
     return (
         <>
 
@@ -37,25 +25,24 @@ export default function ItemWindow({itemWindowState, item, closeItemWindow, colu
                 <>
                 <span onClick={closeItemWindow} className='itemWindowBackground'>Item Window Background</span>
                 <div id={columnId} ref={itemWindow} className='itemWindow' >
-                    {/* <h2>{content}</h2>     */}
-                    <form  >
+
+                    <form >
                         <h2>
-                            <textarea ref={changeTitleForm} onChange={changeTitle} value={content} />
+                            <textarea maxLength={28} id="formTitle" ref={changeTitleForm} onChange={changeTitle} value={content} />
                         </h2>    
                     </form>
-                    {/* <p>{description}</p> */}
 
                     <hr />
                     {description.length > 0 ?  
                         <>
                             <form  >
-                                <textarea ref={addDescriptionForm} onChange={()=>addNewDescription("description")} value={description} />
+                                <textarea ref={addDescriptionForm} onChange={addNewDescription} value={description} />
                             </form>
                         </>
                     : 
-                    <>
+                        <>
                             <form >
-                                <textarea  aria-controls='none' onChange={()=>addNewDescription("description")} ref={addDescriptionForm} placeholder="Add Description"/>
+                                <textarea  aria-controls='none' onChange={addNewDescription} ref={addDescriptionForm} placeholder="Add Description"/>
                             </form>
                             <span>Add Description</span>
                         </>
